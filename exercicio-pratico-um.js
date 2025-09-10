ca092463189e520fbca85485cc3e7c73c6a63643
Kevin Santos <kevin_s_santos@estudante.sc.senai.br>
2025-09-10 23:54:15 +0000

    adiciona arquivo condicionais.js com lógica de aprovação e reprovacao

---
+++ exercicio-pratico-um.js
@@ -0,0 +1,8 @@
+let meuNome = "Kevin";
+let minhaIdade = 17;
+let minhaCidade = "Pomerode - SC"
+let gostaDeProgramar = true;
+
+console.log(`Perfil: ${meuNome}, ${minhaIdade} anos`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log("Gosta de programar: " ,gostaDeProgramar);
