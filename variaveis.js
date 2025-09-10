b231a7563988309a37f322fbf9e0a3b5f3decc91
KevinSSantos <kevin_s_santos@estudante.sc.senai.br>
2025-09-10 19:33:31 -0300

    Declaração de variaveis e descritivo da diferença entre javascript e  portugol

---
+++ variaveis.js
@@ -0,0 +1,25 @@
+//Portugol para JavaScript
+//Declaração de variáveis
+
+/*
+Em Portugol
+cadeia nome = "Kevin"
+cadeia sobrenome = "Santos"
+inteiro idade = 24
+real altura = 1.71
+real peso = 69
+logico ehHumano = verdadeiro
+*/
+
+
+let nome = "Kevin"
+let sobrenome = "Santos"
+let idade = 24
+let altura = 1.71
+let peso = 69
+
+console.log("Nome:", nome)
+console.log("Sobrenome:", sobrenome)
+console.log("Idade:", idade)
+console.log("Altura:", altura)
+console.log("Peso:", peso)
